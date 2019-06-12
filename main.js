417d1dec26d7728e56270a4bb9081d7cf7775076
Dante Calderon <dantehemerson@gmail.com>
2019-06-11 21:43:26 -0500

    Feat #2 Update tags

--- main.js
+++ main.js
@@ -12,9 +12,29 @@ async function getTags(filename) {
   }
 }
 
+async function updateTags(filename, tags) {
+  try {
+    const file = await readFile(filename)
+    return NodeID3.update(tags, file)
+  } catch (e) {
+    console.log(`Cannt update tags.`, e)
+  }
+}
+
 async function main() {
   const tags = await getTags(FILENAME_TEST)
+  console.log(`Old tags: `)
   console.log(tags)
+
+  console.log(`Tags for update`)
+  const newTags = {
+    title: `Title ${Math.random() * 10}`,
+  }
+  console.log(newTags)
+
+  const resUpdate = await updateTags(FILENAME_TEST, newTags)
+  console.log(`Los nuevos tags son: `)
+  console.log(NodeID3.read(resUpdate))
 }
 
 main()
