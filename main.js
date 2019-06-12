5081d62c251a428914724c2d9ec5eb94cf3e5312
Dante Calderon <dantehemerson@gmail.com>
2019-06-11 21:19:40 -0500

    Remove unused function

--- main.js
+++ main.js
@@ -3,16 +3,6 @@ const NodeID3 = require('node-id3')
 
 const FILENAME_TEST = './files/audio.mp3'
 
-async function readAudioFile(filename) {
-  let file
-  try {
-    file = await readFile(filename)
-  } catch (e) {
-    console.log(`Can't read file`, e)
-  }
-  return file
-}
-
 async function getTags(filename) {
   try {
     const file = await readFile(filename)
