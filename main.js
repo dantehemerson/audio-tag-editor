b54fc12eb388ee0fd694fefa077ee0b2a650a1f2
Dante Calderon <dantehemerson@gmail.com>
2019-06-11 21:57:06 -0500

    Save file with new tags. Closes #3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { readFile } = require('./src/lib/fs')
+const { readFile, writeFile } = require('./src/lib/fs')
 const NodeID3 = require('node-id3')
 
 const FILENAME_TEST = './files/audio.mp3'
@@ -21,6 +21,19 @@ async function updateTags(filename, tags) {
   }
 }
 
+async function saveFile(buffer, filename) {
+  filename =
+    filename || `./result/generated-ate-${new Date().toISOString()}.mp3`
+  try {
+    const res = await writeFile(filename, buffer)
+    return res
+  } catch (e) {
+    console.log(`Error on save file`, e)
+  } finally {
+    console.log(`File saved correctly with name ${filename}`)
+  }
+}
+
 async function main() {
   const tags = await getTags(FILENAME_TEST)
   console.log(`Old tags: `)
@@ -33,8 +46,11 @@ async function main() {
   console.log(newTags)
 
   const resUpdate = await updateTags(FILENAME_TEST, newTags)
-  console.log(`Los nuevos tags son: `)
+  console.log(`The new tags are: `)
   console.log(NodeID3.read(resUpdate))
+
+  console.log(`Guardando archivo.`)
+  console.log(await saveFile(resUpdate))
 }
 
 main()
--- src/lib/fs.js
+++ src/lib/fs.js
@@ -2,3 +2,4 @@ const fs = require('fs')
 const { promisify } = require('util')
 
 exports.readFile = promisify(fs.readFile)
+exports.writeFile = promisify(fs.writeFile)
