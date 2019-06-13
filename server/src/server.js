ff13b16eef89406f5405585adb4d7a75560a61a6
Dante Calderon <dantehemerson@gmail.com>
2019-06-12 23:33:29 -0500

    Get tags of audio file

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,46 @@
 const restify = require('restify')
 const NodeID3 = require('node-id3')
 const { getTags } = require('./lib/audio')
+const restifyPromise = require('restify-await-promise')
+
+const server = restify.createServer()
+
+//Allows you to manipulate the errors before restify does its work
+const alwaysBlameTheUserErrorTransformer = {
+  transform: function(exceptionThrownByRoute) {
+    //Always blame the user
+    exceptionThrownByRoute.statusCode = 400
+    return exceptionThrownByRoute
+  },
+}
+
+const options = {
+  logger: console.log, //Optional: Will automatically log exceptions
+  errorTransformer: alwaysBlameTheUserErrorTransformer, //Optional: Lets you add status codes
+}
+
+restifyPromise.install(server, options)
 
 function respond(req, res, next) {
   res.send('hello ' + req.params.name)
   next()
 }
 
-const server = restify.createServer()
-
-server.use(restify.plugins.bodyParser())
+server.use(restify.plugins.multipartBodyParser())
 
 server.get('/hello/:name', respond)
 
-server.post('/upload', function(req, res) {
+server.post('/upload', async function(req, res, next) {
   console.log(`El archivo de audio es `)
   console.log(req.files)
-  const tags = NodeID3.read(req.files.audio_file)
-
+  const uploadedFile = req.files.audio_file
+  const filename = uploadedFile.path
+  console.log(`El nombre del archivo es `, filename)
+  const tags = await getTags(filename)
   res.json({
     tags,
   })
+  next()
 })
 
 server.listen(8080, function() {
