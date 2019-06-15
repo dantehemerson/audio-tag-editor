d466dd873558baf560d4a0f226cf374b4bffd028
Dante Calderon <dantehemerson@gmail.com>
2019-06-15 17:45:18 -0500

    Edit input

--- web/src/components/Edit.js
+++ web/src/components/Edit.js
@@ -4,13 +4,24 @@ export default class Edit extends React.Component {
   constructor(...props) {
     super(...props)
     this.state = {
-      data: this.props.data,
+      ...this.props.data,
     }
   }
 
+  handleChange = event => {
+    this.setState({ title: event.target.value })
+  }
+
   render() {
-    console.log(this.state.data)
-    console.log(`Titulo `, this.state.data.title)
-    return <div>{this.state.data.title}</div>
+    return (
+      <div>
+        <span>Title</span>
+        <input
+          type="text"
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+      </div>
+    )
   }
 }
