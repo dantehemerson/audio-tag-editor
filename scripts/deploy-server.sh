8543ce80f11186e24c7044553b9da85e928ebde2
Dante Calderon <dantehemerson@gmail.com>
2019-06-28 23:36:33 -0500

    Fix comment typo

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -1,2 +1,2 @@
-# Deploy only the page server to heroku
+# Deploy only the folder server(where is the server, duuhh) to heroku
 git push heroku `git subtree split --prefix server master`:master --force
